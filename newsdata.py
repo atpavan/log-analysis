d11a1364322a62b21c09368000deb9ec9db8286a
Pavan Vutla <pavanb120660bt@gmail.com>
2020-03-21 20:22:55 +0530

    Added the templates and routing file

---
+++ newsdata.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+from newsdb import most_popular_articles, most_popular_authors, request_errors
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def index():
+    return render_template("links.html")
+
+@app.route("/top-articles", methods=["GET"])
+def top_articles():
+    
+    return render_template("list.html", items=most_popular_articles())
+
+@app.route("/top-authors", methods=["GET"])
+def top_authors():
+    
+    return render_template("list.html", items=most_popular_authors())
+
+@app.route("/failed-reqs", methods=["GET"])
+def failed_reqs():
+
+    return render_template("failed_reqs.html", items=request_errors())
+
+if __name__ == "__main__":
+    app.run(port=8000)
+    
